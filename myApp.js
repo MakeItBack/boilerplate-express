47e66929ae02b4f76eb0d4eb3c769e1442167e72
Mike Back <hello@mikeback.me>
2022-09-13 07:01:15 +1000

    Add 'get' route to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
 let express = require("express");
 let app = express();
+
+/*
+Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
+Be sure that your code works by looking at the logs, then see the results in the preview if you are using Replit.
+*/
+
+app.get("/", function (req, res) {
+   res.send("Hello Express");
+});
 console.log("Hello World");
 
 module.exports = app;
